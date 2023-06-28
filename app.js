f466f9d184ff143637f01f4d5a86e77e0ec3de0c
AMIRARJ <130403371+hajmusi@users.noreply.github.com>
2023-06-28 22:37:11 +0330

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+// let school = {
+//   classA: "ali",
+//   classb: "reza",
+// };
+// console.log(school);
+let school = {
+  classA: {
+    info: {
+      first: "teachername",
+      last: "tedade danesh amuzan",
+    },
+    students: {
+      ali: ["ali", "score:5", "age:17"],
+      reza: ["reza", "score:7", "age:10"],
+      mmd: ["mmd", "score:2", "age:22"],
+    },
+  },
+  classB: {
+    info: {
+      first: "teachername",
+      last: "tedade danesh amuzan",
+    },
+    students: {
+      ali: ["ali", "score:5", "age:17"],
+      reza: ["reza", "score:7", "age:10"],
+      mmd: ["mmd", "score:2", "age:22"],
+    },
+  },
+};
+console.log(school);
